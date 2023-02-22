4f4337484b00a086768bcc6f272b9538241b67f5
WITH-PC2019\with <sg04068@naver.com>
2023-02-22 17:57:29 +0900

    23.02.22 gradle 프로젝트 생성

---
+++ ExToy/src/main/java/com/exampleToy/ExToy/ExToyApplication.java
@@ -0,0 +1,13 @@
+package com.exampleToy.ExToy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExToyApplication.class, args);
+	}
+
+}
---
+++ ExToy/src/test/java/com/exampleToy/ExToy/ExToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleToy.ExToy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExToyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
